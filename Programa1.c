9f80b3e7a62a203de882377bdfb4ed2232e27ffc
Emmanuel Sánchez <emmsanchezaranda@hotmail.com>
2022-06-03 18:05:06 -0500

    Tabla de multiplcicar e ignorar .exe

--- Programa1.c
+++ Programa1.c
@@ -2,7 +2,15 @@
 
 void main()
 {
-    int A, B, C;
+    int A,j, i;
+    
+    for (i = 1; i < 11; i++)
+    {
+        for (j = 1; j < 11; j++)
+        {
+            printf("%d X %d = %d\n", i, j, i*j);
+        }
+    }
     
 
     return;
